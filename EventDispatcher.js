c915b33772242d481c85906d508f5cb1550eaa08
Serge Karankevich <crashet@gmail.com>
2014-11-27 23:20:10 +0300

    Main functionality is realised

--- EventDispatcher.js
+++ EventDispatcher.js
@@ -2,10 +2,28 @@ function EventDispatcher() {
     this.subscriptions = {};
 }
 
-EventDispatcher.prototype.subscribe = function () {
+EventDispatcher.prototype.subscribe = function (eventName, callbackFunction) {
+    var eventSubscriptions = this.subscriptions[eventName];
 
+    if (typeof eventSubscriptions === 'undefined') {
+        eventSubscriptions = this.subscriptions[eventName] = [];
+    }
+
+    if (typeof callbackFunction !== 'function') {
+        return;
+    }
+
+    eventSubscriptions.push(callbackFunction);
 };
 
-EventDispatcher.prototype.trigger = function () {
+EventDispatcher.prototype.trigger = function (eventName, data) {
+    var eventSubscriptions = this.subscriptions[eventName];
 
+    if (typeof eventSubscriptions === 'undefined') {
+        return;
+    }
+
+    for (var index in eventSubscriptions) {
+        eventSubscriptions[index](data, eventName);
+    }
 };
