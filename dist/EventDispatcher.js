fcb50f9a80d4e130c9a4c04bb0dfd7dabbc28f61
Serge Karankevich <ksv@onliner.by>
2015-04-23 15:47:51 +0300

    module.exports support is added

--- dist/EventDispatcher.js
+++ dist/EventDispatcher.js
@@ -1,36 +1,46 @@
-function EventDispatcher() {
-    this.subscriptions = {};
-}
-
-EventDispatcher.prototype.subscribe = function (eventName, callbackFunction) {
-    var eventSubscriptions = this.subscriptions[eventName];
-
-    if (typeof eventSubscriptions === 'undefined') {
-        eventSubscriptions = this.subscriptions[eventName] = [];
+(function (root, constructor) {
+    if (typeof module === "object" && typeof module.exports === "object") {
+        module.exports = constructor;
+    } else {
+        root.EventDispatcher = constructor;
+    }
+}(this, (function () {
+    function EventDispatcher() {
+        this.subscriptions = {};
     }
 
-    if (typeof callbackFunction !== 'function') {
-        return;
-    }
+    EventDispatcher.prototype.subscribe = function (eventName, callbackFunction) {
+        var eventSubscriptions = this.subscriptions[eventName];
 
-    eventSubscriptions.push(callbackFunction);
-};
+        if (typeof eventSubscriptions === 'undefined') {
+            eventSubscriptions = this.subscriptions[eventName] = [];
+        }
 
-EventDispatcher.prototype.trigger = function (eventName) {
-    var callbackArguments = Array.prototype.splice.call(arguments, 1),
-        eventSubscriptions = this.subscriptions[eventName];
+        if (typeof callbackFunction !== 'function') {
+            return;
+        }
 
-    if (typeof eventSubscriptions === 'undefined') {
-        return;
-    }
+        eventSubscriptions.push(callbackFunction);
+    };
 
-    for (var index in eventSubscriptions) {
-        eventSubscriptions[index].apply(null, callbackArguments);
-    }
-};
+    EventDispatcher.prototype.trigger = function (eventName) {
+        var callbackArguments = Array.prototype.splice.call(arguments, 1),
+            eventSubscriptions = this.subscriptions[eventName];
 
-EventDispatcher.prototype.unsubscribe = function (eventName) {
-    if (typeof this.subscriptions[eventName] !== 'undefined') {
-        delete this.subscriptions[eventName];
-    }
-};
+        if (typeof eventSubscriptions === 'undefined') {
+            return;
+        }
+
+        for (var index in eventSubscriptions) {
+            eventSubscriptions[index].apply(null, callbackArguments);
+        }
+    };
+
+    EventDispatcher.prototype.unsubscribe = function (eventName) {
+        if (typeof this.subscriptions[eventName] !== 'undefined') {
+            delete this.subscriptions[eventName];
+        }
+    };
+
+    return EventDispatcher;
+}())));
